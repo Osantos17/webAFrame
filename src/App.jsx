8a2d3ebedd2098ec088ddd42a8da850652a25b75
Orlando Santos <Santos.Orlando17@gmail.com>
2025-02-04 01:41:31 -0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
       <header className=" text-white py-4">
         <div className="container mx-auto text-center">
           <img 
-            src="/public/Whitelogo.png" 
+            src="/src/assets/Whitelogo.png" 
             className="my-4 mx-auto w-28 h-28" 
             alt="White Logo"
           />
@@ -41,22 +41,22 @@ const LandingPage = () => {
         </section>
         <div className="flex flex-row justify-between mt-6 mx-12">
           <img 
-            src="/public/map.png" 
+            src="/src/assets/map.png" 
             className="w-40" 
             alt="map"
           />
           <img 
-            src="/public/local.png" 
+            src="/src/assets/local.png" 
             className="w-40" 
             alt="local"
           />
           <img 
-            src="/public/forecast.png" 
+            src="/src/assets/forecast.png" 
             className="w-40" 
             alt="forecast"
           />
           <img 
-            src="/public/about.png" 
+            src="/src/assets/about.png" 
             className="w-40" 
             alt="about"
           />
